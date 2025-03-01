cc2f3c45ff4d5fe386559ed6acd5f20552b9e95f
Alexey Kudelko <flexivanov237@gmail.com>
2025-03-01 14:00:32 +0300

    refactored files arch

--- src/text_check_workers/files_map.rs
+++ src/text_check_workers/files_map.rs
@@ -1,7 +1,7 @@
 use phf::phf_map;
 
 pub static FILES: phf::Map<&'static str, &'static str> = phf_map! {
-    "sexual" => r"C:\Users\User\Desktop\bad_words_filter\bad_words_filter\sexual.txt",
-    "strong" => r"C:\Users\User\Desktop\bad_words_filter\bad_words_filter\stongswords.txt",
+    "sexual" => r"C:\Users\User\Desktop\bad_words_filter\bad_words_filter\filter_data\sexual.txt",
+    "strong" => r"C:\Users\User\Desktop\bad_words_filter\bad_words_filter\filter_data\stongswords.txt",
     //"political" => r"C:\Users\User\Desktop\bad_words_filter\bad_words_filter\political.txt",
 };
